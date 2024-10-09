6e13c1d88b38cbfffd9af07cd15f2dc031b0ed0c
Alex Hortua <alexander@datzsolutions.com>
2024-10-09 16:09:08 -0600

    Adding some extra CSS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,9 @@ export default function Home() {
   return (
     <div className="flex min-w-96 justify-around">
       <div>
-        <h1>Posts</h1>
+        <h1 className="font-bold">Posts</h1>
       </div>
       <div>
-        <h1>Topics</h1>
         <CreateTopicForm />
       </div>
     </div>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
 import { Navbar, NavbarBrand, NavbarContent, Input } from '@nextui-org/react';
 import HeaderAuth from './header-auth';
+import Link from 'next/link';
+import { path } from '@/paths';
 
 export default async function Header() {
   return (
-    <Navbar shouldHideOnScroll>
+    <Navbar shouldHideOnScroll className="mb-10">
       <NavbarBrand>
-        <p className="font-bold text-inherit">Brandiita</p>
+        <Link href={path.homePage()} className="font-bold text-inherit">
+          Brandiita
+        </Link>
       </NavbarBrand>
-      <NavbarContent className="hidden sm:flex gap-4" justify="center">
-        <Input />
+      <NavbarContent className="w-1/4 gap-4" justify="center">
+        <Input className="w-full" />
       </NavbarContent>
       <NavbarContent className="justify-end" justify="end">
         <HeaderAuth />
